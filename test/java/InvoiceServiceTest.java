6810111d9ede160851f11f5ec4354591c346d689
Pratik Chaudhari <pratikc1020@gmail.com>
2021-07-26 12:10:35 +0530

    Added ability to calculate total fare and minimum fare

---
+++ test/java/InvoiceServiceTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InvoiceServiceTest {
+    @Test
+    void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(25,fare);
+    }
+
+    @Test
+    void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5,fare);
+    }
+}
