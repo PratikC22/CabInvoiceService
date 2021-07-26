7ae65ee84ad8ea580ee259286bfc0105d307256f
Pratik Chaudhari <pratikc1020@gmail.com>
2021-07-26 12:38:11 +0530

    Added invoice service

--- test/java/InvoiceServiceTest.java
+++ test/java/InvoiceServiceTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Hashtable;
+
 public class InvoiceServiceTest {
     @Test
     void givenDistanceAndTime_ShouldReturnTotalFare() {
@@ -37,5 +39,29 @@ public class InvoiceServiceTest {
         InvoiceSummary summary = invoiceGenerator.calculateFareSummary(rides);
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30);
         Assertions.assertEquals(expectedInvoiceSummary, summary);
+    }
+
+    @Test
+    public void givenUserID_ShouldReturnInvoiceSummary() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Hashtable<Integer, Ride[]> hTable = new Hashtable<>();
+
+        int userID1 = 1;
+        Ride[] ride1 = { new Ride(2.0, 5),
+                new Ride(0.1, 1) };
+        hTable.put(userID1, ride1);
+
+        int userID2 = 2;
+        Ride[] ride2 = { new Ride(4.0, 10),
+                new Ride(1, 1) };
+        hTable.put(userID2, ride2);
+
+        int userID = 2;
+
+        if(hTable.containsKey(userID)) {
+            InvoiceSummary summary = invoiceGenerator.calculateFareSummary(hTable.get(userID));
+            InvoiceSummary expectdInvoiceSummary = new InvoiceSummary(2, 61);
+            Assertions.assertEquals(expectdInvoiceSummary, summary);
+        }
     }
 }
